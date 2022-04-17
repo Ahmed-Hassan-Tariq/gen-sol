95b9513835a1bb4631397a15604971a53dcc9b99
Ahmed Hassan <atariq344@gmail.com>
2022-04-17 15:17:28 +0500

    headings

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,19 +8,29 @@ export const Header = () => {
         <Navbar/>
         <div className='header_content' id='home'>
         
-          <div className='header_logo'><img src={logo} alt='logo'/> <button>Get Started</button></div>
-          <div className='header_title'><h1>Heading</h1>
-          <p>Fusce porttitor bibendum massa, at dapibus nisl aliquam eu.
-          Cras laoreet elementum elit in mattis.
-          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          Aenean viverra mattis maximus.
-          Suspendisse vel lorem metus.
-          Curabitur hendrerit blandit sollicitudin.
-          Nullam varius sapien ligula, a tempor ligula porttitor varius.
-          Etiam luctus, libero et facilisis egestas, mauris ipsum tempor odio, ut interdum augue elit at augue.
-          Cras tempus egestas velit, sed dapibus ex gravida a.
-          Morbi ex risus, imperdiet a sem quis, cursus volutpat mauris.</p>
-          
+          <div className='header_logo'>
+
+          <img src={logo} alt='logo'/> 
+          <button>Get Started</button>
+
+          </div>
+
+
+
+          <div className='header_title'>
+            <div className='header_title_content'>
+              <h1>Heading</h1>
+              <p>Fusce porttitor bibendum massa, at dapibus nisl aliquam eu.
+              Cras laoreet elementum elit in mattis.
+              Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              Aenean viverra mattis maximus.
+              Suspendisse vel lorem metus.
+              Curabitur hendrerit blandit sollicitudin.
+              Nullam varius sapien ligula, a tempor ligula porttitor varius.
+              Etiam luctus, libero et facilisis egestas, mauris ipsum tempor odio, ut interdum augue elit at augue.
+              Cras tempus egestas velit, sed dapibus ex gravida a.
+              Morbi ex risus, imperdiet a sem quis, cursus volutpat mauris.</p>
+            </div>
           </div>
 
         </div>
